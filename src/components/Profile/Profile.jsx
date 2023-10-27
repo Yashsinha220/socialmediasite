14bba93da775f998fc58578f7d27e30dcc2b17aa
Yash sinha <84098535+Yashsinha220@users.noreply.github.com>
2023-10-28 01:08:45 +0530

    side bar profile card

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LogoSearch from "../LogoSearch/LogoSearch";
 import "./Profile.css";
+import ProfileCard from "../ProfileCard/ProfileCard";
 
 const Profile = () => {
   return (
     <div className="ProfileSide">
       <LogoSearch />
+      <ProfileCard/>
     </div>
   );
 };
